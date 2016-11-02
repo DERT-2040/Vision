abf3fca1d6c038e5551ff5e0fc85b4e8205a597c
atkindc <david.atkinson@netzero.com>
2016-11-02 18:29:16 -0500

    Simple servo upload

---
+++ simple_servo_v2.0.py
@@ -0,0 +1,32 @@
+# simple_servo_v2.0.py
+
+# use the circuit from "simple_servo.png"
+
+import RPi.GPIO as GPIO
+
+###################################################################################################
+def main():
+    GPIO.setmode(GPIO.BCM)      # use GPIO pin numbering, not physical pin numbering
+
+    led_gpio_pin = 24
+
+    GPIO.setup(led_gpio_pin, GPIO.OUT)
+
+    pwmObject = GPIO.PWM(led_gpio_pin, 100)         # frequency = 100 Hz
+
+    pwmObject.start(14)             # initial duty cycle = 14%
+
+    try:
+        while True:
+            strAngle = raw_input("enter desired angle (0 to 180): ")
+            intAngle = int(strAngle)
+            dutyCycle = ((float(intAngle) * 0.01) + 0.5) * 10
+            pwmObject.ChangeDutyCycle(dutyCycle)
+    # end while
+
+        return
+    except:
+            GPIO.cleanup()
+###################################################################################################
+if __name__ == "__main__":
+    main()
